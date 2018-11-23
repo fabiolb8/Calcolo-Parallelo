20fdb03eabceca0b15e6a2de02b3fb20d52ee5ac
Paolo Maione <plmn_private@hotmail.it>
2018-11-24 00:23:17 +0100

    Esercizio 15 OK su Visual Studio

--- esercizi/esercizio_15/esercizio_15.c
+++ esercizi/esercizio_15/esercizio_15.c
@@ -3,12 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define up		0
-#define down	1
-#define left	2
-#define right	3
 #define dimensioni	2
-#define num_vicini	4
 #define q 3		//griglia 3x3
 #define m 4
 #define n 4
@@ -25,19 +20,15 @@ int main(int argc, char *argv[])
 	int somma, count;
 	float media;
 	char processor_name[MPI_MAX_PROCESSOR_NAME];
-	//int mio_valore;
 	int coords[dimensioni];
 	int dims[dimensioni] = { q,q };
-	int free_coords[2];
-	//int periods[dim]={1,1};				//Caso griglia periodica
+	int free_coords[2];							//Caso griglia periodica
 	int periods[dimensioni] = { 0,0 };			//Caso griglia non periodica
-	//int vicini[num_vicini];
-	//float* valore_medie;
-	double** matrice;
-	double* data_matrice;
-	double **sotto_matrice_per_righe, **sotto_matrice_per_colonne_e_righe;
-	double *data_sotto_matrice_per_righe, *data_sotto_matrice_per_colonne_e_righe;
-	int *dimRecv_righe, *dimRecv_colonne, *displs_righe, *displs_colonne;
+	double** matrice=NULL;
+	double* data_matrice = NULL;
+	double **sotto_matrice_per_righe = NULL, **sotto_matrice_per_colonne_e_righe = NULL;
+	double *data_sotto_matrice_per_righe = NULL, *data_sotto_matrice_per_colonne_e_righe = NULL;
+	int *dimRecv_righe = NULL, *dimRecv_colonne = NULL, *displs_righe = NULL, *displs_colonne = NULL;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
@@ -47,7 +38,7 @@ int main(int argc, char *argv[])
 	if (myrank == 0) {
 
 		//Controllo numprocs coerente con definizione griglia
-		if (numprocs != q*q) {
+		if (numprocs != q * q) {
 			printf("Errore! Il numero dei processi deve essere %d\n", q*q);
 			MPI_Abort(MPI_COMM_WORLD, 1);
 		}
@@ -74,9 +65,6 @@ int main(int argc, char *argv[])
 		MPI_Barrier(griglia);
 	}
 
-	/*if(myrank==0){
-		valore_medie = (float*) malloc (numprocs*sizeof(float));
-	}*/
 
 	matrice = (double**)malloc(m * sizeof(double*));
 	if (myrank == 0) {
@@ -125,7 +113,6 @@ int main(int argc, char *argv[])
 
 
 		//Inizializzazione dimRecv_righe per scatterv
-
 		for (i = 0; i < q; i++) {
 
 			if (i < ((int)m%q)) {
@@ -138,9 +125,9 @@ int main(int argc, char *argv[])
 			}
 		}
 
-		for (i = 0; i < numprocs; i++) {
+		for (i = 0; i < q; i++) {
 
-			if (i < (n%q)) {
+			if (i < ((int)n%q)) {
 				//Devo ricevere i+1 elementi
 				dimRecv_colonne[i] = (int)(n / q + 1);
 			}
@@ -168,27 +155,28 @@ int main(int argc, char *argv[])
 	}
 
 
-	//Broadcast su col_comm
-	MPI_Bcast(dimRecv_righe, q, MPI_INT, 0,MPI_COMM_WORLD);
+	//Broadcast
+	MPI_Bcast(dimRecv_righe, q, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Bcast(displs_righe, q, MPI_INT, 0, MPI_COMM_WORLD);
-
-
-	//Broadcast su row_comm
+	//Broadcast
 	MPI_Bcast(dimRecv_colonne, q, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Bcast(displs_colonne, q, MPI_INT, 0, MPI_COMM_WORLD);
 
+	//Calcolo dei rank locali nelle sottogriglie
 	MPI_Comm_rank(col_comm, &myrank_col);
 	MPI_Comm_rank(row_comm, &myrank_row);
 
-	sotto_matrice_per_righe = (double**)malloc(dimRecv_righe[myrank_col]/n * sizeof(double*));
 	
+	//FASE 1 : SCATTERV PER RIGHE NELLA PRIMA COLONNA DELLA GRIGLIA
+	
+	sotto_matrice_per_righe = (double**)malloc(dimRecv_righe[myrank_col] / n * sizeof(double*));
+
 	//se sto sulla prima colonna della griglia invio la matrice per righe
 	if (coords[1] == 0) {
 
-		
 		//Allocazione matrice da distribuire
 		data_sotto_matrice_per_righe = (double*)malloc(dimRecv_righe[myrank_col] * sizeof(double));
-		for (i = 0; i < dimRecv_righe[myrank_col]/n; i++) {
+		for (i = 0; i < dimRecv_righe[myrank_col] / n; i++) {
 			sotto_matrice_per_righe[i] = &(data_sotto_matrice_per_righe[i*n]);
 		}
 
@@ -196,9 +184,9 @@ int main(int argc, char *argv[])
 		//printMat(sotto_matrice_per_righe, dimRecv_righe[myrank], n, myrank);
 
 
-		MPI_Scatterv(&matrice[0][0],dimRecv_righe,displs_righe,MPI_DOUBLE,&sotto_matrice_per_righe[0][0],dimRecv_righe[myrank_col],MPI_DOUBLE,0,col_comm);
-	
-		printMat(sotto_matrice_per_righe,dimRecv_righe[myrank_col]/n,n,myrank);
+		MPI_Scatterv(&matrice[0][0], dimRecv_righe, displs_righe, MPI_DOUBLE, &sotto_matrice_per_righe[0][0], dimRecv_righe[myrank_col], MPI_DOUBLE, 0, col_comm);
+
+		printMat(sotto_matrice_per_righe, dimRecv_righe[myrank_col] / n, n, myrank);
 
 	}
 
@@ -206,124 +194,72 @@ int main(int argc, char *argv[])
 
 	//Definizione nuovi tipi per la colonna da inviare
 	MPI_Datatype acol, acoltype;
-	MPI_Type_vector(dimRecv_righe[myrank_col], 1, n, MPI_DOUBLE, &acol);
+	MPI_Type_vector(dimRecv_righe[myrank_col]/n, 1, n, MPI_DOUBLE, &acol);
 	MPI_Type_commit(&acol);
 	MPI_Type_create_resized(acol, 0, 1 * sizeof(double), &acoltype);
 	MPI_Type_commit(&acoltype);
 
 	//Definizione nuovi tipi per la colonna da ricevere
 	MPI_Datatype ccol, ccoltype;
-	MPI_Type_vector(dimRecv_righe[myrank_col], 1, dimRecv_colonne[myrank_row], MPI_DOUBLE, &ccol);
+	MPI_Type_vector(dimRecv_righe[myrank_col]/n, 1, dimRecv_colonne[myrank_row], MPI_DOUBLE, &ccol);
 	MPI_Type_commit(&ccol);
 	MPI_Type_create_resized(ccol, 0, 1 * sizeof(double), &ccoltype);
 	MPI_Type_commit(&ccoltype);
 
-	
+	MPI_Barrier(griglia);
 
-	//tutti devono avere questo
-	sotto_matrice_per_colonne_e_righe = (double**)malloc(dimRecv_righe[myrank_col]/n* sizeof(double*));
-	//Allocazione matrice da distribuire
-		data_sotto_matrice_per_colonne_e_righe = (double*)malloc(dimRecv_colonne[myrank_row]*(dimRecv_righe[myrank_col]/n)*sizeof(double));
-		for (i = 0; i < dimRecv_righe[myrank_col] / n; i++) {
-			sotto_matrice_per_colonne_e_righe[i] = &(data_sotto_matrice_per_colonne_e_righe[i*dimRecv_colonne[myrank_row]]);
-		}
+	//FASE 2 : SCATTERV PER COLONNE NELLE SOTTOGRIGLIE DI RIGHE
+
+	//Allocazione blocco in cui ricevere
+	sotto_matrice_per_colonne_e_righe = (double**)malloc(dimRecv_righe[myrank_col] / n * sizeof(double*));
+	data_sotto_matrice_per_colonne_e_righe = (double*)malloc(dimRecv_colonne[myrank_row] * (dimRecv_righe[myrank_col] / n) * sizeof(double));
+	for (i = 0; i < dimRecv_righe[myrank_col] / n; i++) {
+		sotto_matrice_per_colonne_e_righe[i] = &(data_sotto_matrice_per_colonne_e_righe[i*dimRecv_colonne[myrank_row]]);
+	}
 
 
-	MPI_Scatterv(&sotto_matrice_per_righe[0][0], dimRecv_colonne, displs_colonne, acoltype, &sotto_matrice_per_colonne_e_righe[0][0], dimRecv_colonne[myrank_row]* (dimRecv_righe[myrank_col]/n), ccoltype, 0, row_comm);
+
+	MPI_Scatterv(&sotto_matrice_per_righe[0][0], dimRecv_colonne, displs_colonne, acoltype, &sotto_matrice_per_colonne_e_righe[0][0], dimRecv_colonne[myrank_row] * (dimRecv_righe[myrank_col] / n), ccoltype, 0, row_comm);
 
 	printMat(sotto_matrice_per_colonne_e_righe, dimRecv_righe[myrank_col] / n, dimRecv_colonne[myrank_row], myrank);
 
 
-
-
-	//Inizializzazione valore da distribuire
-	//mio_valore = myrank;
-
-	//Get rank dei miei vicini
-	//MPI_Cart_shift(griglia, 0, 1, &vicini[up], &vicini[down]);
-	//MPI_Cart_shift(griglia, 1, 1, &vicini[left], &vicini[right]);
-
-	//Stampa in ordine dei processi
-	/*for (i = 0; i < numprocs; i++) {
-
-		if (myrank == i) {
-			printf("Ciao sono il processo %d. I rank dei miei vicini sono U:%d, D:%d, L:%d, R:%d\n", myrank, vicini[up], vicini[down], vicini[left], vicini[right]);
-		}
-		MPI_Barrier(griglia);
-	}*/
-
-
-	/*
-	//Calcolo media rank vicini
-	somma = mio_valore;
-	count = 1;
-	for (i=0; i<num_vicini; i++){
-
-		if (vicini[i]>=0){
-			somma+=vicini[i];
-			count++;
-		}
-	}
-	media = (float) somma/count;
-	*/
-
-
-	/*
-
-	//Stampa in ordine
-	for (i = 0; i < numprocs; i++) {
-
-		if (myrank == i) {
-			printf("Ciao sono il processo %d. La media dei rank dei miei vicini e' %f\n", myrank, media);
-		}
-		MPI_Barrier(griglia);
-	}
-
-
-	//Mando le medie a 0
-	MPI_Gather(&media, 1, MPI_FLOAT, valore_medie, 1, MPI_FLOAT, 0, griglia);
-
-	if (myrank==0){
-
-
-		printf("Ciao sono il processo %d. Le medie sono: ", myrank);
-
-		for (i = 0; i < numprocs; i++)
-			printf("%f\n",valore_medie[i]);
-
-		printf("\n");
-
-	}
-
 	//Libero la memoria nel processo root
-	if(myrank==0)
-		free(valore_medie);
-
-
-	*/
-
-	
-	//if (myrank == 0) {
-
+	if (myrank == 0) {
+		free(matrice);
+		free(data_matrice);
+	}
 		
-		printf("dimRecv_righe:\n");
-		printVett(dimRecv_righe, q, myrank);
+	//Libero la memoria in tutti i processi
+	free(dimRecv_colonne);
+	free(dimRecv_righe);
+	free(displs_colonne);
+	free(displs_righe);
+	free(sotto_matrice_per_colonne_e_righe);
+	free(data_sotto_matrice_per_colonne_e_righe);
+	free(sotto_matrice_per_righe);
+	free(data_sotto_matrice_per_righe);
 
-		printf("dimRecv_colonne:\n");
-		printVett(dimRecv_colonne, q, myrank);
+	MPI_Type_free(&acol);
+	MPI_Type_free(&acoltype);
+	MPI_Type_free(&ccol);
+	MPI_Type_free(&ccoltype);
+
+	MPI_Barrier(griglia);
+
+	/*
+	printf("dimRecv_righe:\n");
+	printVett(dimRecv_righe, q, myrank);
+
+	printf("dimRecv_colonne:\n");
+	printVett(dimRecv_colonne, q, myrank);
 
 
-		printf("displs_righe:\n");
-		printVett(displs_righe, q, myrank);
+	printf("displs_righe:\n");
+	printVett(displs_righe, q, myrank);
 
-		printf("displs_colonne:\n");
-		printVett(displs_colonne, q, myrank);
-		
-
-	
-
-
-
+	printf("displs_colonne:\n");
+	printVett(displs_colonne, q, myrank);*/
 
 	MPI_Finalize();
 	return 0;
@@ -365,3 +301,4 @@ void printMat(double** data, int r, int c, int rank) {
 		printf("\n");
 	}
 }
+
