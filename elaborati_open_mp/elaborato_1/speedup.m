a7b893334150fe427d3fd4779b7625eb62a3d035
Paolo Maione <plmn_private@hotmail.it>
2018-12-04 11:51:08 +0100

    fine elaborato 1 openMP

--- elaborati_open_mp/elaborato_1/speedup.m
+++ elaborati_open_mp/elaborato_1/speedup.m
@@ -3,10 +3,10 @@
 warning('OFF', 'MATLAB:table:ModifiedAndSavedVarnames');
 p=[2,4,8];
 tabella_speedup = readtable('test_valutazione_integrale.xlsx', 'Range', 'B9:G12','ReadRowName', true);
-tempi_img = imread('tempi.png');
+speedup_img = imread('speedup.png');
 figure(1)
-imshow(tempi_img)
-title('Tempi')
+imshow(speedup_img)
+title('Speedup')
 
 %% ANALISI PRESTAZIONI TEMPI
 
@@ -14,7 +14,7 @@ c = [table2array(tabella_speedup(:,1)),table2array(tabella_speedup(:,2)),table2a
 figure(2)
 plot(p,c(:,1),'-*',p,c(:,2),'-o',p,c(:,3),'-+',p,c(:,4),'-gs',p,c(:,5),'-md',p,p,'--','LineWidth',2)
 title('Analisi speed up')
-legend('N=10^4','N=10^5','N=10^6','N=10^7','N=10^8')
+legend('N=10^4','N=10^5','N=10^6','N=10^7','N=10^8','Location','NorthWest')
 xlim([p(1) p(3)])
 xlabel('Numero di thread');
 ylabel('Speedup S(p)');
