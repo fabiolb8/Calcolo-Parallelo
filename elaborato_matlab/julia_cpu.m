e674a20c18e0b7c2d3924bdb5974c97b0edaeca8
Fabio <lobruttofabio8@gmail.com>
2018-12-09 18:21:23 +0100

    Codici frattali

---
+++ elaborato_matlab/julia_cpu.m
@@ -0,0 +1,18 @@
+function tempo = julia_cpu(x,c,n,iterazioni)
+%x=1.5, c=-0.75, n=400, iterazioni=20
+% insieme Julia set
+T=tic;
+f = @(x) x.^2+c;
+xval=linspace(-x,x,n);
+yval=xval;
+[X,Y]=meshgrid(xval,yval);
+Z=X+1i*Y;
+for k=1:iterazioni
+	Z=f(Z);
+end
+W = exp(-abs(Z));
+tempo=toc(T);
+imagesc(W);
+colormap(prism(256))
+shading flat;
+axis('square','equal','off');
---
+++ elaborato_matlab/julia_gpu.m
@@ -0,0 +1,19 @@
+function tempo = julia_gpu(x,c,n,iterazioni)
+%x=1.5, c=-0.75, n=400, iterazioni=20
+% insieme Julia set
+T=tic;
+f = @(x) x.^2+c;
+xval=gpuArray.linspace(-x,x,n);
+yval=xval;
+[X,Y]=meshgrid(xval,yval);
+Z=gpuArray(X+1i*Y);
+for k=1:iterazioni
+	Z=f(Z);
+end
+W = exp(-abs(Z));
+W = gather(W);
+tempo=toc(T);
+imagesc(W);
+colormap(prism(256))
+shading flat;
+axis('square','equal','off');
